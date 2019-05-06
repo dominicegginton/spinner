75a2cadc9b0e5014b4df67bbe9c87f1f26114065
Dominic Egginton <dominic.egginton@gmail.com>
2019-05-06 18:15:39 +0100

    added custom single and multi frame patterns

--- Sources/Spinner/Pattern.swift
+++ Sources/Spinner/Pattern.swift
@@ -2,11 +2,23 @@ public enum Pattern {
 
     case dots
     case lines
+    case singleFrame(String)
+    case multiFrame([String])
+
+    public init(singleFrame: String) {
+        self = .singleFrame(singleFrame)
+    }
+
+    public init(multiFrame: [String]) {
+        self = .multiFrame(multiFrame)
+    } 
 
     public var frames: [String] {
         switch self {
             case .dots: return ["⠋","⠙","⠹","⠸","⠼","⠴","⠦","⠧","⠇","⠏"]
             case .lines: return ["-","\\","|","/"]
+            case .singleFrame(let singleFrame): return [singleFrame]
+            case .multiFrame(let multiFrame): return multiFrame
         }
     }
 
@@ -14,6 +26,8 @@ public enum Pattern {
         switch self {
             case .dots: return 0.08
             case .lines: return 0.08
+            case .singleFrame(_): return 1
+            case .multiFrame(_): return 0.08
         }
     }
 }
