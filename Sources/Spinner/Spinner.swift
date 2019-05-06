c99495bc418cdf2e63e49efb95c89d66c08bf55e
Dominic Egginton <dominic.egginton@gmail.com>
2019-05-06 17:00:22 +0100

    added basic spinner class

--- Sources/Spinner/Spinner.swift
+++ Sources/Spinner/Spinner.swift
@@ -1,3 +1,19 @@
-struct Spinner {
-    var text = "Hello, World!"
+import Foundation
+import Dispatch
+
+public class Spinner {
+    
+    var pattern: Pattern
+    var text: String
+    
+    var running: Bool
+    var frameIndex: Int
+    var speed: Double
+    var queue: DispatchQueue = DispatchQueue(label: "io.Swift.Spinner")
+
+    public init() {}
+
+    public start() {}
+
+    public stop() {}
 }
