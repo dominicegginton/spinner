8dca9cc16c354db59125c1720d8422a111759e59
Dominic Egginton <dominic.egginton@gmail.com>
2019-05-07 10:48:41 +0100

    added completion handler enum to spinner file

--- Sources/Spinner/Spinner.swift
+++ Sources/Spinner/Spinner.swift
@@ -96,3 +96,22 @@ public class Spinner {
 
     }
 }
+
+public enum completionHandler {
+    
+    case success
+    case failure
+    case warning
+    case information
+    case custom(Pattern)
+
+    public var pattern: Pattern {
+        switch self {
+            case .success: return Pattern(singleFrame: "✔".green)
+            case .failure: return Pattern(singleFrame: "✖".red)
+            case .warning: return Pattern(singleFrame: "⚠".yellow)
+            case .information: return Pattern(singleFrame: "ℹ".blue)
+            case .custom(let pattern): return pattern
+        }
+    }
+}
