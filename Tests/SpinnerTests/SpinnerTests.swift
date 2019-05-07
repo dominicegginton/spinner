33f479adb98efa0214f256559e660795e304004d
Dominic Egginton <dominic.egginton@gmail.com>
2019-05-07 17:26:21 +0100

    added some simple tests

--- Tests/SpinnerTests/SpinnerTests.swift
+++ Tests/SpinnerTests/SpinnerTests.swift
@@ -2,14 +2,37 @@ import XCTest
 @testable import Spinner
 
 final class SpinnerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Spinner().text, "Hello, World!")
+    
+    func testSpinnerState() {
+        var testSpinner = Spinner(.dots, "testSpinner")
+        XCTAssertEqual(false, testSpinner.running)
+        testSpinner.start()
+        XCTAssertEqual(true, testSpinner.running)
+        testSpinner.stopAndClear()
+        XCTAssertEqual(false, testSpinner.running)
+    }
+
+    func testSpinnerText() {
+        var testSpinner = Spinner(.dots, "testSpinner")
+        XCTAssertEqual("testSpinner", testSpinner.text)
+        testSpinner.start()
+        testSpinner.updateText("Spinner")
+        XCTAssertEqual("Spinner    ", testSpinner.text)
+        testSpinner.stopAndClear()
+    }
+
+    func testFrames() {
+        let testPattern = Pattern(multiFrame: ["a","b","c","d"])
+        let testSpinner = Spinner(testPattern, "testSpinner")
+        
+        XCTAssertEqual(testSpinner.currentFrame(), "a")
+        XCTAssertEqual(testSpinner.currentFrame(), "b")
+        XCTAssertEqual(testSpinner.currentFrame(), "c")
+        XCTAssertEqual(testSpinner.currentFrame(), "d")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSpinnerState", testSpinnerState),
+        ("testSpinnerText", testSpinnerText),
     ]
 }
