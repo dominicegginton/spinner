d47c588c1579316aa85f8685342cf10a8344311b
Dominic Egginton <dominic.egginton@gmail.com>
2023-03-18 22:27:48 +0000

    deps: bump nanoseconds to version 1.1.2

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "Spinner", targets: ["Spinner"])
     ],
     dependencies: [
-        .package(url: "https://github.com/dominicegginton/Nanoseconds", from: "1.0.0"),
+        .package(url: "https://github.com/dominicegginton/Nanoseconds", from: "1.1.2"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
         .package(url: "https://github.com/IBM-Swift/BlueSignals.git", from: "1.0.0")
     ],
